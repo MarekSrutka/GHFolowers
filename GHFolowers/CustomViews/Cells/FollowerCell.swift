fd64776762f7a4f8897de851f5d8eab01b205a84
MarekSrutka <spiritecz@icloud.com>
2024-02-29 18:13:16 +0100

    refactor custom components, downloadImage, emptyState and LoadingScreen

--- GHFolowers/CustomViews/Cells/FollowerCell.swift
+++ GHFolowers/CustomViews/Cells/FollowerCell.swift
@@ -27,7 +27,10 @@ class FollowerCell: UICollectionViewCell {
     
     func set(follower: Follower) {
         usernameLabel.text = follower.login
-        avatarImageView.donwloadImage(from: follower.avatarUrl)
+        NetworkManager.share.downloadImage(from: follower.avatarUrl) { [weak self] image in
+            guard let self = self else { return }
+            DispatchQueue.main.async { self.avatarImageView.image = image }
+        }
     }
     
     private func configureAvatarImageView() {
